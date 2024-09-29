f129a2ce7889e53f805120f090331435bf991764
George Thorne <your_email@example.com>
2024-09-29 16:42:13 +0300

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,3 +170,16 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+  /**
+   * EmailJs intergration
+   */
+
+  function sendMail(){
+    let parms = {
+      name : document.getElementById("name").value
+      email : document.getElementById("email").value
+      subject : document.getElementById("subject").value
+      message : document.getElementById("message").value
+    }
+  }
